31c0c8f8d6a33e654db2a53d698294ec9fe2becb
Matthew <matthewjoseph.bilaos@msugensan.edu.ph>
2023-06-01 13:01:29 +0800

    Comment Resident Trigger and Views. Change table->database

--- fatima-complaints.sql
+++ fatima-complaints.sql
@@ -1,4 +1,4 @@
-CREATE TABLE complaintsc;
+CREATE DATABASE complaintsc;
 USE complaintsc;
 
 CREATE TABLE `resident` (
@@ -379,16 +379,16 @@ GRANT INSERT, SELECT, UPDATE ON complaintsc.mediator TO 'clerk_complaint_admin'@
 
 -- TRIGGERS
 
-DELIMITER //
-CREATE TRIGGER before_resident_delete
-BEFORE DELETE ON resident
-FOR EACH ROW
-BEGIN
-    INSERT INTO resident_archive (resident_id, first_name, mid_name, last_name, suffix, sex, date_of_birth, place_of_birth, civil_status, nationality, occupation, religion, blood_type, fourps_status, disability_status, type_disability, senior_status, educational_attainment, phone_number, tel_number, email, purok, street, lot_number, voter_status, voter_id, precinct_number, national_id, vaccine_status, vaccine_1, vaccine_date_1, vaccine_2, vaccine_date_2, booster_status, booster_1, booster_date_1, booster_2, booster_date_2, emergency_person, relationship, emergency_address, emergency_contact, img_url, alien_status, deceased_status, date_of_death, created_by, date_created, updated_by, date_updated, remarks, archived_by, date_archived)
-    VALUES (OLD.resident_id, OLD.first_name, OLD.mid_name, OLD.last_name, OLD.suffix, OLD.sex, OLD.date_of_birth, OLD.place_of_birth, OLD.civil_status, OLD.nationality, OLD.occupation, OLD.religion, OLD.blood_type, OLD.fourps_status, OLD.disability_status, OLD.type_disability, OLD.senior_status, OLD.educational_attainment, OLD.phone_number, OLD.tel_number, OLD.email, OLD.purok, OLD.street, OLD.lot_number, OLD.voter_status, OLD.voter_id, OLD.precinct_number, OLD.national_id, OLD.vaccine_status, OLD.vaccine_1, OLD.vaccine_date_1, OLD.vaccine_2, OLD.vaccine_date_2, OLD.booster_status, OLD.booster_1, OLD.booster_date_1, OLD.booster_2, OLD.booster_date_2, OLD.emergency_person, OLD.relationship, OLD.emergency_address, OLD.emergency_contact, OLD.img_url, OLD.alien_status, OLD.deceased_status, OLD.date_of_death, OLD.created_by, OLD.date_created, OLD.updated_by, OLD.date_updated, NULL, NULL, current_timestamp());
-END//
-DELIMITER ;
-
+-- DELIMITER //
+-- CREATE TRIGGER before_resident_delete
+-- BEFORE DELETE ON resident
+-- FOR EACH ROW
+-- BEGIN
+--    INSERT INTO resident_archive (resident_id, first_name, mid_name, last_name, suffix, sex, date_of_birth, place_of_birth, civil_status, nationality, occupation, religion, blood_type, fourps_status, disability_status, type_disability, senior_status, educational_attainment, phone_number, tel_number, email, purok, street, lot_number, voter_status, voter_id, precinct_number, national_id, vaccine_status, vaccine_1, vaccine_date_1, vaccine_2, vaccine_date_2, booster_status, booster_1, booster_date_1, booster_2, booster_date_2, emergency_person, relationship, emergency_address, emergency_contact, img_url, alien_status, deceased_status, date_of_death, created_by, date_created, updated_by, date_updated, remarks, archived_by, date_archived)
+--    VALUES (OLD.resident_id, OLD.first_name, OLD.mid_name, OLD.last_name, OLD.suffix, OLD.sex, OLD.date_of_birth, OLD.place_of_birth, OLD.civil_status, OLD.nationality, OLD.occupation, OLD.religion, OLD.blood_type, OLD.fourps_status, OLD.disability_status, OLD.type_disability, OLD.senior_status, OLD.educational_attainment, OLD.phone_number, OLD.tel_number, OLD.email, OLD.purok, OLD.street, OLD.lot_number, OLD.voter_status, OLD.voter_id, OLD.precinct_number, OLD.national_id, OLD.vaccine_status, OLD.vaccine_1, OLD.vaccine_date_1, OLD.vaccine_2, OLD.vaccine_date_2, OLD.booster_status, OLD.booster_1, OLD.booster_date_1, OLD.booster_2, OLD.booster_date_2, OLD.emergency_person, OLD.relationship, OLD.emergency_address, OLD.emergency_contact, OLD.img_url, OLD.alien_status, OLD.deceased_status, OLD.date_of_death, OLD.created_by, OLD.date_created, OLD.updated_by, OLD.date_updated, NULL, NULL, current_timestamp());
+-- END//
+-- DELIMITER ;
+--
 DELIMITER //
 CREATE TRIGGER before_complaint_delete
 BEFORE DELETE ON complaint
@@ -412,10 +412,10 @@ DELIMITER ;
 -- VIEWS
 
 -- View to retrieve basic information of residents.
-CREATE VIEW resident_info AS
-SELECT resident_id, first_name, last_name, date_of_birth, occupation, email
-FROM resident;
-
+-- CREATE VIEW resident_info AS
+-- SELECT resident_id, first_name, last_name, date_of_birth, occupation, email
+-- FROM resident;
+--
 -- View to get details of complaints along with the names of complainants and respondents
 CREATE VIEW complaint_details AS
 SELECT c.case_no, r1.first_name AS complainant_first_name, r1.last_name AS complainant_last_name,
